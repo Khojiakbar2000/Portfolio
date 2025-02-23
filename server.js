737f0c506dc1cf4a64e23f8de62547e51ca441d6
Khojiakbar2000 <ishonkulovkhojiakbar@gmail.com>
2025-02-23 13:58:56 -0800

    Event loop and callback function

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(express.urlencoded({extended:true}));
 //2: Session code 
 
 //3: Views code
-app.set(":view","views");
+app.set("views","views");
 app.set('view engine',"ejs");
 
 //4 Routing code
@@ -44,5 +44,3 @@ let PORT = 3000;
 server.listen(PORT , function(){
     console.log(`The server is running successfully on port: ${PORT}`)
 });
-
-

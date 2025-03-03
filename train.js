e631d688ac4379e94818b5b1a2ddca3b2a54c748
Khojiakbar2000 <ishonkulovkhojiakbar@gmail.com>
2025-03-02 18:29:52 -0800

    C-TASK

--- train.js
+++ train.js
@@ -1,6 +1,6 @@
 //MITASK
 function checkContent(str1, str2){
-return str1.split("").sort().join("") === str2.split("").sort().join("")
+return str1.split("").sort().join("") === str2.split("").sort().join("");
 
 }
 
